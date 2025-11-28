107bccb399032c6859b6a744f841cba717b7f30d
Muhammad Hishaam Bin Saghir <m.hishaambinsaghir@gmail.com>
2025-11-28 04:43:36 -0800

    rechange the vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     // 1. CRITICAL: Set the base path to your repo name (e.g., /my-project/)
-    base: '/your-repository-name/',
+    base: '/SocialSphere/',
 
     // 2. IMPORTANT: Tell Vite to use the 'docs' folder for output
     build: {
